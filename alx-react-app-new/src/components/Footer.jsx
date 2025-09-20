6528ad19d680b2ee669a4490c5395ed5a697d77d
haradejene <haradejeneleta@gmail.com>
2025-09-20 07:27:19 -0700

    added styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Footer(){
+return(
+<footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+  <p>© 2023 City Lovers</p>
+</footer>)}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function MainContent(){
+return(
+<main style={{ color: 'grey' }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+)
+}
+
+export default MainContent;
