9ea34f7842da32ea92e19acf18625335f76c11d1
haradejene <haradejeneleta@gmail.com>
2025-10-04 15:04:32 -0700

    commit

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { searchUsers } from "../services/githubService";
+import { fetchUserData, searchUsers } from "../services/githubService";
 
 export default function Search() {
   const [username, setUsername] = useState("");
