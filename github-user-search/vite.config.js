6bddaa780598af2b1cf116ac9ff1e8a43eae65ee
haradejene <haradejeneleta@gmail.com>
2025-10-05 01:41:18 -0700

    added some configuration to vite

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-        react(),
-        tailwindcss(), 
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',       // make sure the output folder is 'dist'
+    minify: 'esbuild',    // minifies JS for production
+    sourcemap: false,     // disable source maps for production
+    cssCodeSplit: true,   // split CSS into smaller files
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // optional, ensures optimal chunking
+      },
+    },
+  },
 })
