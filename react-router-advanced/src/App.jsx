aa41334ef8092f90fe45dcef746c8a31be8305ba
haradejene <haradejeneleta@gmail.com>
2025-10-19 22:24:53 +0300

    added BrowserRouter inside App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,45 +1,20 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Profile from './pages/Profile'
-import ProfileDetails from './pages/ProfileDetails'
-import ProfileSettings from './pages/ProfileSettings'
-import Blog from './pages/Blog'
-import Post from './pages/Post'
-import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/profile">Profile</Link> | 
-        <Link to="/blog">Blog</Link>
+        <Link to="/about">About</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-
-        {/* Protected Routes */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-
-        {/* Dynamic Route */}
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:postId" element={<Post />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   )
 }
 
