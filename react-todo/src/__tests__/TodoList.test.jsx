01f8434f0c72d608ceeed9b130906f5d44f56d87
haradejene <haradejeneleta@gmail.com>
2025-10-19 23:15:23 +0300

    created react todo list

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,38 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import TodoList from '../components/TodoList'
+
+describe('TodoList Component', () => {
+  test('renders initial todos', () => {
+    render(<TodoList />)
+    expect(screen.getByText('Learn React')).toBeInTheDocument()
+    expect(screen.getByText('Build a Todo App')).toBeInTheDocument()
+  })
+
+  test('adds a new todo', () => {
+    render(<TodoList />)
+    const input = screen.getByPlaceholderText('Add new todo')
+    const addButton = screen.getByText('Add')
+
+    fireEvent.change(input, { target: { value: 'Write Tests' } })
+    fireEvent.click(addButton)
+
+    expect(screen.getByText('Write Tests')).toBeInTheDocument()
+  })
+
+  test('toggles todo completion', () => {
+    render(<TodoList />)
+    const todo = screen.getByText('Learn React')
+    fireEvent.click(todo)
+    expect(todo).toHaveStyle('text-decoration: line-through')
+    fireEvent.click(todo)
+    expect(todo).toHaveStyle('text-decoration: none')
+  })
+
+  test('deletes a todo', () => {
+    render(<TodoList />)
+    const deleteButton = screen.getByTestId(/delete-1/)
+    fireEvent.click(deleteButton)
+    expect(screen.queryByText('Learn React')).not.toBeInTheDocument()
+  })
+})
